a332e0adc7145128ca729a563a31342a997430b3
Kamaljeet <kamaljeetsingh1088@gmail.com>
2025-06-09 00:21:47 +0530

    update sdk version

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -8,7 +8,7 @@ import Foundation
 import flutter_inappwebview_macos
 import package_info_plus
 import path_provider_foundation
-import sqflite
+import sqflite_darwin
 import url_launcher_macos
 import video_player_avfoundation
 import wakelock_plus
